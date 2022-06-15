c811828dc581d2de33a9277b8b391cdbc3eef193
FOUDRE\Foudre II <laurent.digitick@gmail.com>
2022-06-15 20:39:44 +0200

    nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,19 +4,19 @@ import { NavLink } from 'react-router-dom';
 const Navigation = () => {
     return (
         <>
-            <NavLink to="/">
+            <NavLink to="rega_laurent_wealthhealth_05062022/">
              HRnet
             </NavLink>
             
             <nav>
                 <ul className="nav justify-content-end">
                     <li className="nav-item">
-                        <NavLink to="/" className={(nav) => nav.isActive ? "nav-active" : ""}>
+                        <NavLink to="rega_laurent_wealthhealth_05062022/" className={(nav) => nav.isActive ? "nav-active" : ""}>
                             Home
                         </NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink to='/employees' className={(nav) => nav.isActive ? "nav-active" : ""}>
+                        <NavLink to='rega_laurent_wealthhealth_05062022/employees' className={(nav) => nav.isActive ? "nav-active" : ""}>
                             Employees List
                         </NavLink>
                     </li>
